98b3be5964d3bab4a487177e2f63c1bebfddadcd
frodrigues1706 <69855686+frodrigues1706@users.noreply.github.com>
2020-10-06 11:24:09 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Felipe Manoel Rodrigues"
+    return "Meu primeiro pipeline"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
